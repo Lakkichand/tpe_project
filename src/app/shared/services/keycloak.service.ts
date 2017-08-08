3f190a7a856ccc8471c60434e63733f7325c7de9
venkatshekhar <shekhar.shetty1992@gmail.com>
2017-08-08 14:03:49 +0530

    service rename and assets added

---
+++ src/app/shared/services/keycloak.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+
+declare var Keycloak: any;
+
+@Injectable()
+export class KeycloakService {
+
+
+  static auth: any = {};
+
+  static logoutRedirectURI = "/index.html";
+
+  static init(): Promise<any> {
+    let keycloakAuth: any = new Keycloak('./config/keycloak.json');
+    KeycloakService.auth.loggedIn = false;
+
+    return new Promise((resolve, reject) => {
+      keycloakAuth.init({ onLoad: 'login-required' })
+        .success(() => {
+          KeycloakService.auth.loggedIn = true;
+          KeycloakService.auth.authz = keycloakAuth;
+          KeycloakService.auth.logoutUrl = keycloakAuth.createLogoutUrl({ redirectUri: window.location.protocol + "//" + window.location.host + KeycloakService.logoutRedirectURI });
+          resolve(null);
+        })
+        .error(() => {
+          reject(null);
+        });
+    });
+  }
+
+  getUserDetails() {
+    return KeycloakService.auth;
+  }
+
+  static logout() {
+    KeycloakService.auth.loggedIn = false;
+    KeycloakService.auth.authz = null;
+
+    window.location.href = KeycloakService.auth.logoutUrl;
+  }
+
+  getToken(): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      if (KeycloakService.auth.authz.token) {
+        KeycloakService.auth.authz.updateToken(5).success(function () {
+          resolve(<string>KeycloakService.auth.authz.token);
+        })
+          .error(function () {
+            reject('AUTH_ERROR');
+            KeycloakService.logout();
+          });
+      }
+    });
+  }
+
+}
---
+++ src/app/shared/services/user-detail.service.ts
@@ -0,0 +1,34 @@
+
+import { Injectable } from "@angular/core";
+import { KeycloakService } from "./keycloak.service";
+
+@Injectable()
+export class UserDetailService {
+
+    userDetails: any;
+    isHOAuser: boolean = false;
+    isSiteManagerUser: boolean = false;
+
+
+    constructor(private keycloakService: KeycloakService) {
+        this.setUserDetails();
+    }
+
+    setUserDetails() {
+        let roles = this.keycloakService.getUserDetails().authz.realmAccess.roles;
+        this.isHOAuser = roles.some(x => x === "hoa");
+        this.isSiteManagerUser = roles.some(x => x === "site_manager");
+
+        // this.keycloakService.getUserDetails().authz.loadUserInfo().success(function (data) {
+        //     this.userDetails = data.name;
+        // })
+
+        console.log('isHOAuser :', this.isHOAuser );
+        console.log('isSiteManagerUser :', this.isSiteManagerUser );
+        // console.log('userDetails :', this.userDetails );
+
+    }
+
+
+
+}
